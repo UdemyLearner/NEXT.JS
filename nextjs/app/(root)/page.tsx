691caa954001d76d77750cf87ad45d46f3164e38
प्रियांशु नरेडी <104293796+UdemyLearner@users.noreply.github.com>
2025-03-10 00:59:08 +0000

    Added MetaData for Home Page

--- nextjs/app/(root)/page.tsx
+++ nextjs/app/(root)/page.tsx
@@ -1,12 +1,17 @@
 // import Image from "next/image";
 import Hello from "../components/hello";
 
+export const metadata = {
+  title : "Home | Next.js",
+  description : "Home Page"
+}; 
+
 export default function Home() {
   console.log("What am I doing here? -- SERVER OR CLIENT?");
   
   return (
     <>
-    <h1>Welcome to NEXT.JS</h1>
+    <h1 className="text-3xl">Welcome to NEXT.JS</h1>
     <Hello />
     </>
   );
